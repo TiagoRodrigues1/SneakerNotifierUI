8020398653259bb504b7d6f9644c626b0579566f
Tiago Rodrigues <tiaagorodrigues1999@gmail.com>
2023-08-28 18:48:24 +0100

    add searchbar funcionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
 "use client"
+import { useEffect, useState } from "react";
 import NavBar from "@components/NavBar";
 import Feed from "@components/Feed";
 
 const Home = () => {
+
+  const [products, setProducts] = useState([]);
+  const [searchProducts, setSearchProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("api/products");
+      const data = await response.json();
+
+      setProducts(data);
+      setSearchProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <section className="container mx-auto">
-      <NavBar />
+      <NavBar products = {products} setSearchProducts = {setSearchProducts} />
       <div className="ml-20 mt-5">
-        <Feed />
+        <Feed products={searchProducts}/>
       </div>
     </section>
   );
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,20 +1,8 @@
 "use client"
-import { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 import Sneaker from "@models/sneaker";
 
-function Feed() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const response = await fetch("api/products");
-      const data = await response.json();
-
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+function Feed({products} : {products : Sneaker[]}) {
 
   return (
     <div className="grid grid-cols-3">
